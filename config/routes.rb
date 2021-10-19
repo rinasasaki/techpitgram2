6c98a21676f4b85a9a9273c7b54e20d771b21e3d
佐々木　莉奈 <1072sasaki@gmail.com>
2021-10-20 06:46:30 +0900

    [add]newアクション createアクションのルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,9 @@ Rails.application.routes.draw do
 
   get '/users/:id', to: 'users#show', as: 'user'
   # usersコントローラーのshowアクションのルーティング
+
+  get '/posts/new', to: 'posts#new'
+  post '/posts', to: 'posts#create'
+  post '/posts/:post_id/photos', to: 'photos#create', as: 'post_photos'
+  #postsコントローラーのnewアクション。投稿ページを表示するルーティング、 postsコントローラーのcreateアクション。投稿を作成するルーティング、photosコントローラーのcreateアクション。投稿の写真を保存するルーティング
 end
