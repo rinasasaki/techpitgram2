0a47d86c65ff5822f77a74b1cd5ad0c1abad6d28
佐々木　莉奈 <1072sasaki@gmail.com>
2021-10-20 06:49:15 +0900

    [fix] resourceを使用したルーティングに変更j

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,8 @@ Rails.application.routes.draw do
   get '/users/:id', to: 'users#show', as: 'user'
   # usersコントローラーのshowアクションのルーティング
 
-  get '/posts/new', to: 'posts#new'
-  post '/posts', to: 'posts#create'
-  post '/posts/:post_id/photos', to: 'photos#create', as: 'post_photos'
-  #postsコントローラーのnewアクション。投稿ページを表示するルーティング、 postsコントローラーのcreateアクション。投稿を作成するルーティング、photosコントローラーのcreateアクション。投稿の写真を保存するルーティング
+  resources :posts, only: %i(new create) do
+    resources :photos, only: %i(create)
+  end
+  
 end
