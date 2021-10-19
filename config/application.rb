6692861d49c52e16fd765f82aadbdbec57e56828
佐々木　莉奈 <1072sasaki@gmail.com>
2021-10-20 07:17:57 +0900

    [fix] 日本時間に変更

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ module Techpitgram
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
+    config.i18n.default_locale = :ja
+    #日本時間に変更
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
