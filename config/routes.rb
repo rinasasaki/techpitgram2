8b7348e22c1a0943cc9f3069766deafed076141d
佐々木　莉奈 <1072sasaki@gmail.com>
2021-10-20 07:21:57 +0900

    [fix] 投稿ページの見た目を整える

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, # ここの行にカンマを追加
     controllers: { registrations: 'registrations' } # ここの行を追加
 
-  root 'pages#home'
+    root 'posts#index'
 
   get '/users/:id', to: 'users#show', as: 'user'
   # usersコントローラーのshowアクションのルーティング
