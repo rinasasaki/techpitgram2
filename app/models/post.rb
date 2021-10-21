cd6af0a476a5804927bfaeba53e5fef2d0ba1e54
佐々木　莉奈 <1072sasaki@gmail.com>
2021-10-21 23:55:59 +0900

    [add] Likeモデル側のアソシエーション設定を追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,4 +5,6 @@ class Post < ApplicationRecord
   has_many :photos, dependent: :destroy
 
   accepts_nested_attributes_for :photos
+
+  has_many :likes, -> { order(created_at: :desc) }, dependent: :destroy
 end
