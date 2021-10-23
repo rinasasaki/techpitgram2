fa331d35f273e41504886a87dda876b52cd40e25
佐々木　莉奈 <1072sasaki@gmail.com>
2021-10-24 00:29:55 +0900

    [fix]コメントのモデルを設定

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,6 +5,15 @@ class Post < ApplicationRecord
   has_many :photos, dependent: :destroy
 
   has_many :likes, -> { order(created_at: :desc) }, dependent: :destroy
+  
+  has_many :comments, dependent: :destroy
+
+  accepts_nested_attributes_for :photos
+
+  def liked_by(current_user)
+    # user_idが一致するlikeを検索する
+    Like.find_by(user_id: current_user.id, post_id: id)
+  end
 
   accepts_nested_attributes_for :photos
 
