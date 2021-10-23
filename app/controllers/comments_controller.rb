83a390e60f9467c46e25cc0f845be48147cfac37
佐々木　莉奈 <1072sasaki@gmail.com>
2021-10-24 00:33:14 +0900

    [add] コントローラーにdestroyアクションを追加

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -10,11 +10,22 @@ class CommentsController < ApplicationController
         flash[:alert] = "コメントに失敗しました"
       end
     end
+
+    def destroy
+      @comment = Comment.find_by(id: params[:id])
+      @post = @comment.post
+      if @comment.destroy
+        respond_to :js
+      else
+        flash[:alert] = "コメントの削除に失敗しました"
+      end
+    end
+    
   
     private
       def comment_params
         params.required(:comment).permit(:user_id, :post_id, :comment)
       end
   end
-  
+
 end
