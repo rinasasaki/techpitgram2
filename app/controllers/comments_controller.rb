bbbc0ac809715467c8fbcea85360fac0d3bf5882
佐々木　莉奈 <1072sasaki@gmail.com>
2021-10-24 00:32:27 +0900

    [fix]コメントコントローラーを修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,20 @@
 class CommentsController < ApplicationController
+
+  class CommentsController < ApplicationController
+    def create
+      @comment = Comment.new(comment_params)
+      @post = @comment.post
+      if @comment.save
+        respond_to :js
+      else
+        flash[:alert] = "コメントに失敗しました"
+      end
+    end
+  
+    private
+      def comment_params
+        params.required(:comment).permit(:user_id, :post_id, :comment)
+      end
+  end
+  
 end
