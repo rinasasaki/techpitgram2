98e0785f8e546e0649d9c507cc3c70f75ca4d472
佐々木　莉奈 <1072sasaki@gmail.com>
2021-10-25 15:27:41 +0900

    [add] local: falseを追加

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,7 +6,6 @@
       @comment = Comment.new(comment_params)
       @post = @comment.post
       if @comment.save
-        respond_to :js
       else
         flash[:alert] = "コメントに失敗しました"
       end
