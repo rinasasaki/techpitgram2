520da27d74d961273396631470946bb9358517f1
佐々木　莉奈 <1072sasaki@gmail.com>
2021-10-25 20:46:52 +0900

    [add] respond_to :jsを追加

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,6 +6,7 @@
       @comment = Comment.new(comment_params)
       @post = @comment.post
       if @comment.save
+        respond_to :js
       else
         flash[:alert] = "コメントに失敗しました"
       end
