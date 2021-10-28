91e31a93d673970f0dff1c1e3fff695393074afc
佐々木　莉奈 <1072sasaki@gmail.com>
2021-10-28 22:30:10 +0900

    [fix]usernameカラムを保存できるように編集

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,7 @@ class ApplicationController < ActionController::Base
 
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :username])
+      #usernameカラムを保存できるように編集
       devise_parameter_sanitizer.permit(:account_update, keys: [:name])
     end
     
