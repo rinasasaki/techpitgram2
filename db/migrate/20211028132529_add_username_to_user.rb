69411ff66fb9e5301698d0d2e1b71e831fdfe584
佐々木　莉奈 <1072sasaki@gmail.com>
2021-10-28 22:27:09 +0900

    [add] マイグレーションファイルを実行

--- db/migrate/20211028132529_add_username_to_user.rb
+++ db/migrate/20211028132529_add_username_to_user.rb
@@ -1,5 +1,5 @@
 class AddUsernameToUser < ActiveRecord::Migration[6.1]
   def change
-    add_column :users, :username, :string
+    add_column :users, :username, :string, null: false, default: ""
   end
 end
